15e47e5834194107402fafbc90c0f6a1e9c9673c
Fahimul Islam <fahimulislam93@gmail.com>
2018-09-18 04:17:32 +0600

    Event Data Added on the firebase

--- app/src/main/java/com/tourmate/com/tourmate/FragmentCreateEvent.java
+++ app/src/main/java/com/tourmate/com/tourmate/FragmentCreateEvent.java
@@ -3,35 +3,50 @@ package com.tourmate.com.tourmate;
 import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.content.DialogInterface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-public class AddEventActivity extends AppCompatActivity {
-    EditText mDestination, mBudget, mFromDate, mToDate;
-    Button mSaveEvent, mFromDateButton, mToDateButton;
+public class FragmentCreateEvent extends Fragment {
+    private EditText mDestination, mBudget, mFromDate, mToDate;
+    private Button mSaveEvent, mFromDateButton, mToDateButton;
+    private DatabaseReference rootRef;
+    private DatabaseReference userRef;
+    private DatabaseReference databaseTravelEvents;
+    private FirebaseUser user;
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_event);
-        mDestination = findViewById(R.id.et_travel_destination);
-        mBudget = findViewById(R.id.et_estimated_budget);
-        mFromDate = findViewById(R.id.et_from_date);
-        mToDate = findViewById(R.id.et_to_date);
-        mSaveEvent = findViewById(R.id.btnEventSave);
-        mFromDateButton = findViewById(R.id.btn_from_date);
-        mToDateButton = findViewById(R.id.btn_to_date);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.create_event_layout,container,false);
+        mDestination = v.findViewById(R.id.et_travel_destination);
+        mBudget = v.findViewById(R.id.et_estimated_budget);
+        mFromDate = v.findViewById(R.id.et_from_date);
+        mToDate = v.findViewById(R.id.et_to_date);
+        mSaveEvent = v.findViewById(R.id.btnEventSave);
+        mFromDateButton = v.findViewById(R.id.btn_from_date);
+        mToDateButton = v.findViewById(R.id.btn_to_date);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        rootRef = FirebaseDatabase.getInstance().getReference();
+        userRef = rootRef.child("users");
+
 
         mFromDateButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,36 +65,49 @@ public class AddEventActivity extends AppCompatActivity {
         mSaveEvent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DatabaseReference databaseTravelEvents;
-                databaseTravelEvents = FirebaseDatabase.getInstance().getReference("travel_events");
-
-                String destination = mDestination.getText().toString().trim();
-                String budget = mBudget.getText().toString().trim();
-                String fromDate = mFromDate.getText().toString().trim();
-                String toDate = mToDate.getText().toString().trim();
-
-                if (TextUtils.isEmpty(destination)){
-                    mDestination.setError("Please Provide Your Travel Destination");
-                    mDestination.requestFocus();
+                if (user != null){
+                    String uid = user.getUid();
+                    saveEvent(uid);
                 }
-                else if (TextUtils.isEmpty(budget)){
-                    mBudget.setError("Please Provide Your Estimated Budget");
-                    mBudget.requestFocus();
-                }
-                else if (TextUtils.isEmpty(fromDate)){
-                    Toast.makeText(AddEventActivity.this, "Please Select Your Event Start Date", Toast.LENGTH_SHORT).show();
-                }
-                else if (TextUtils.isEmpty(toDate)){
-                    Toast.makeText(AddEventActivity.this, "Please Select Your Event Close Date", Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    String id = databaseTravelEvents.push().getKey();
-                    TravelEvent travelEvent = new TravelEvent(id,destination,budget,fromDate,toDate);
-                    databaseTravelEvents.child(id).setValue(travelEvent);
-                    Toast.makeText(AddEventActivity.this, "Travel Event Added Successfully", Toast.LENGTH_SHORT).show();
-                }
+//                
             }
         });
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("Create New Event");
+    }
+
+    public void saveEvent(String userId){
+        String destination = mDestination.getText().toString().trim();
+        String budget = mBudget.getText().toString().trim();
+        String fromDate = mFromDate.getText().toString().trim();
+        String toDate = mToDate.getText().toString().trim();
+
+        if (TextUtils.isEmpty(destination)){
+            mDestination.setError("Please Provide Your Travel Destination");
+            mDestination.requestFocus();
+        }
+        else if (TextUtils.isEmpty(budget)){
+            mBudget.setError("Please Provide Your Estimated Budget");
+            mBudget.requestFocus();
+        }
+        else if (TextUtils.isEmpty(fromDate)){
+            Toast.makeText(getActivity(), "Please Select Your Event Start Date", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(toDate)){
+            Toast.makeText(getActivity(), "Please Select Your Event Close Date", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            databaseTravelEvents = userRef.child(userId).child("travel_events");
+            String id = databaseTravelEvents.push().getKey();
+            TravelEvent travelEvent = new TravelEvent(id,destination,budget,fromDate,toDate);
+            databaseTravelEvents.child(id).setValue(travelEvent);
+            Toast.makeText(getActivity(), "Travel Event Added Successfully", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void chooseFromDate() {
@@ -88,7 +116,7 @@ public class AddEventActivity extends AppCompatActivity {
         int month = calendar.get(Calendar.MONTH);
         final int day = calendar.get(Calendar.DAY_OF_MONTH);
         DatePickerDialog datePicker =
-                new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(final DatePicker view, final int year, final int month,
                                           final int dayOfMonth) {
@@ -117,7 +145,7 @@ public class AddEventActivity extends AppCompatActivity {
         int month = calendar.get(Calendar.MONTH);
         final int day = calendar.get(Calendar.DAY_OF_MONTH);
         DatePickerDialog datePicker =
-                new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(final DatePicker view, final int year, final int month,
                                           final int dayOfMonth) {
--- app/src/main/java/com/tourmate/com/tourmate/HomeFragment.java
+++ app/src/main/java/com/tourmate/com/tourmate/HomeFragment.java
@@ -10,6 +10,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -21,12 +22,12 @@ public class HomeFragment extends Fragment {
     }
 
     ImageView mCreateEvent, mlocation_imageview;
-
     @Override
     public View onCreateView( LayoutInflater inflater, ViewGroup container,
                               Bundle savedInstanceState ) {
         // Inflate the layout for this fragment
         View v= inflater.inflate(R.layout.fragment_home, container, false);
+
         mCreateEvent = v.findViewById((R.id.event_imageview));
         mlocation_imageview = v.findViewById(R.id.location_imageview);
 
@@ -41,8 +42,10 @@ public class HomeFragment extends Fragment {
         mCreateEvent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(),AddEventActivity.class);
-                startActivity(intent);
+                Fragment mFragment = new FragmentCreateEvent();
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_contineer, mFragment).commit();
+//                Intent intent = new Intent(getActivity(),AddEventActivity.class);
+//                startActivity(intent);
             }
         });
 
